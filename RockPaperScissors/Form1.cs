c99a43a0d499eb5246edb404df023afb68b68cd8
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-05-06 18:18:31 -0500

    Initial Commit

---
+++ RockPaperScissors/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void RockChoice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PaperChoice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ScissorsChoice_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void OpponentSelection()
+        {
+
+        }
+
+        bool VicotoryCheck()
+        {
+            return true;
+        }
+    }
+}

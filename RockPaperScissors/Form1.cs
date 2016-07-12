b3b6161f73356bfce8fff77444b6fb27e208aad5
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-07-12 10:42:44 -0500

    Adding comments for Tutorial
    Defining all components of the tutorial in an easy to read format.

--- RockPaperScissors/Form1.cs
+++ RockPaperScissors/Form1.cs
@@ -17,13 +17,28 @@ namespace RockPaperScissors
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Create two variables of type string
+        /// These variables will be used to hold inforamtion about 
+        /// the option the player choose to play against the computer
+        /// and the same goes for the computer choice
+        /// </summary>
         string UserChoice;
         string OpponentChoice;
 
+        /// <summary>
+        /// This function is all ready attached to our RockChoice button on our form that is provided.
+        /// This will be executed once the RockChoice button has been clicked by the user of our application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void RockChoice_Click(object sender, EventArgs e)
         {
-            UserChoice = RockChoice.Text;
-            OpponentSelection();
+            /// <summary>
+            /// This function will set the value of the variable we create above that we will be using to hold
+            /// our data for the choice the player has made and we will set it equal to Rock.
+            /// Once we have our data stored in that variable we will call the function OpponenentSelection.
+            /// </summary>
         }
 
         private void PaperChoice_Click(object sender, EventArgs e)
@@ -38,29 +53,45 @@ namespace RockPaperScissors
             OpponentSelection();
         }
 
+        /// <summary>
+        /// This function will generate us a choice for our computer player based on some 
+        /// conditions we set for it. We will call this function in the ScissorsChoice_Click, PaperChoice_Click, and RockChoice_Click
+        /// functions above. We call it in those function to make it appear as if the computer and user are selecting thier choose at the 
+        /// same time just like in Rock Paper Scissors.
+        /// </summary>
         void OpponentSelection()
         {
-            Computer.Text = "Opponent Choice";
+            /// <summary>
+            /// To we will need to create some way to get the computer to generate
+            /// a response to the players choice. There are a few ways we can do this
+            /// but we will provide you with one using the Random class. This Random class
+            /// will generate us a number between two digits.
+            /// In this case we want a number between 1 and 3 including 1 and 3.
+            /// </summary>
             Random r = new Random();
             int choice = r.Next(1, 3);
-            if(choice == 1)
-            {
-                OpponentChoice = "Rock";
-            }
-            if(choice == 2)
-            {
-                OpponentChoice = "Paper";
-            }
-            if(choice == 3)
-            {
-                OpponentChoice = "Scissors";
-            }
-            Computer.Text += "\n" + OpponentChoice;
-            VicotoryCheck();
+
+            ///<summary>
+            /// Once we have our number generated we need to create a set of conditionals to
+            /// figure out what the computers response would be. 
+            /// 1 = rock
+            /// 2 = paper
+            /// 3 = scissors
+            /// 
+            /// Once the computers response has been generated we will call the Victory Check function.
+            /// </summary>
         }
 
+
+        /// <summary>
+        /// This function is called from the OpponentSelection function above.
+        /// In this function will check to see if the user beats the computer or if the computer beats the player
+        /// </summary>
         void VicotoryCheck()
         {
+            /// <summary>
+            /// 
+            /// </summary>
             string VictoryOne = "Rock>Scissors";
             string VictoryTwo = "Paper>Rock";
             string VictoryThree = "Scissors>Paper";

533d96f042d05c4c744f599ab15c0a23d1e7f681
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-07-14 09:12:46 -0500

    Commenting Code
    Added the final touches to the tutorial

--- RockPaperScissors/Form1.cs
+++ RockPaperScissors/Form1.cs
@@ -41,16 +41,34 @@ namespace RockPaperScissors
             /// </summary>
         }
 
+        /// <summary>
+        /// This function is all ready attached to our PaperChoice button on our form that is provided.
+        /// This will be executed once the PaperChoice button has been clicked by the user of our application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void PaperChoice_Click(object sender, EventArgs e)
         {
-            UserChoice = PaperChoice.Text;
-            OpponentSelection();
+            /// <summary>
+            /// This function will set the value of the variable we create above that we will be using to hold
+            /// our data for the choice the player has made and we will set it equal to Paper.
+            /// Once we have our data stored in that variable we will call the function OpponenentSelection.
+            /// </summary>
         }
 
+        /// <summary>
+        /// This function is all ready attached to our ScissorsChoice button on our form that is provided.
+        /// This will be executed once the ScissorsChoice button has been clicked by the user of our application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ScissorsChoice_Click(object sender, EventArgs e)
         {
-            UserChoice = ScissorsChoice.Text;
-            OpponentSelection();
+            /// <summary>
+            /// This function will set the value of the variable we create above that we will be using to hold
+            /// our data for the choice the player has made and we will set it equal to Scissors.
+            /// Once we have our data stored in that variable we will call the function OpponenentSelection.
+            /// </summary>
         }
 
         /// <summary>
@@ -90,24 +108,26 @@ namespace RockPaperScissors
         void VicotoryCheck()
         {
             /// <summary>
-            /// 
+            /// So now that we have all the set up for our player and computer to select the option they wish to play
+            /// its time to see who wins. There are multiple ways we can do this using more complex data types like Lists or Arrays
+            /// but to keep it simple we are just gonna make a few string varaibles one for each victory condition.
+            /// we will need one to say Rock beats Scissors, Paper beats Rock, and Scissors beats Paper.
             /// </summary>
-            string VictoryOne = "Rock>Scissors";
-            string VictoryTwo = "Paper>Rock";
-            string VictoryThree = "Scissors>Paper";
 
-            string check = UserChoice + ">" + OpponentChoice;
+            /// <summary>
+            /// Once we have our vicotory conditions defined we need to cocme up with some way to check.
+            /// One way I like to do this since we are using strings for everything is we can make a new string and 
+            /// format it like our vicotry check strings.
+            /// </summary>
 
-            if(check == VictoryOne || check == VictoryTwo || check == VictoryThree )
-            {
-                CombatLog.Text += "Victory: " + UserChoice + " beats " + OpponentChoice + "\n";
-            }
-            else if (UserChoice == OpponentChoice)
-            {
-                CombatLog.Text += "Draw: " + UserChoice + " Same as " + OpponentChoice + "\n";
-            }
-            else
-                CombatLog.Text += "Defeat: " + UserChoice + " lost to " + OpponentChoice + "\n";
+            ///<summary>
+            /// Once we have our strings made one two represent the current choices in play to check against the victory conditions
+            /// and one for each of the three victory types we need to make some more conditional statements using if, else if, and else checks.
+            /// 
+            /// We will use this conditianal statements to see if our string that contains the current play to all the victory type to see if the user or the computer has won.
+            /// We will alson needs to see if there is a draw so if the user and the computer have selected the same choice.
+            /// Then if we did not meet the vicotry conditions or draw conditions, the user has lost and the computer is the winner.
+            /// </summary>
         }
     }
 }

f1366cb425083398bb001a36099dba994d27d8d0
lukeplaisance <luke.plaisance17@gmail.com>
2018-06-08 13:52:33 -0500

    added comments

--- RockPaperScissors/Form1.Designer.cs
+++ RockPaperScissors/Form1.Designer.cs
@@ -97,6 +97,7 @@
             this.Controls.Add(this.RockChoice);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- RockPaperScissors/Form1.cs
+++ RockPaperScissors/Form1.cs
@@ -10,6 +10,35 @@ using System.Windows.Forms;
 
 namespace RockPaperScissors
 {
+//What is VS?:
+//Visual studios is a IDE that is used to debug, edit code, and build applications for the microsoft windows platform.
+
+//What is Windows Form Applications?
+//    A winform is a GUI application that is only for Windows.
+
+//Data Types:
+//	-String : a word that contains characters.Used with “”
+//	-Int : integer/whole number
+//	-float : decimal value
+//	-bool : true or false
+//	-char : single letter on the keyboard.Used with ‘’
+
+//Operators:
+//	+(addition) add
+//	+=(plus equals) the value to the right is added to the value to the left
+//	-(subtraction) subtract
+//	-=(minus equals) value to the right is subtracted to the value to the left
+//	= (is assigned) assignment operator
+//    ==(is equal to)
+//	||(or)
+//	&&(and)
+
+//Methods:
+//	Methods are a block of code that does a specific.You can call it over and over so you don't have to retype the code again
+
+//Classes
+//    Classes lets you group custom variable types and methods together and can define the functionality the object will have
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -77,6 +106,11 @@ namespace RockPaperScissors
             }
             else
                 CombatLog.Text += "Defeat: " + UserChoice + " lost to " + OpponentChoice + "\n";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }

9346a10a3b89016de51514f64af01f4b5ab2ded5
lukeplaisance <luke.plaisance17@gmail.com>
2018-06-08 13:48:35 -0500

    added comments

---
+++ RockPaperScissors/Comments.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    //What is VS?:
+    //    Visual studios is a IDE that is used to debug, edit code, and build applications for the microsoft windows platform.
+
+    //What is Windows Form Applications?
+    //    A winform is a GUI application that is only for Windows.
+
+    //Data Types:
+    //	-String : a word that contains characters.Used with “”
+    //	-Int : integer/whole number
+    //	-float : decimal value
+    //	-bool : true or false
+    //	-char : single letter on the keyboard.Used with ‘’
+
+    //Operators:
+    //	+(addition) add
+    //	+=(plus equals) the value to the right is added to the value to the left
+    //	-(subtraction) subtract
+    //	-=(minus equals) value to the right is subtracted to the value to the left
+    //	= (is assigned) assignment operator
+    //    ==(is equal to)
+    //	||(or)
+    //	&&(and)
+
+    //Methods:
+    //	Methods are a block of code that does a specific.You can call it over and over so you don't have to retype the code again
+
+    //Classes
+    //    Classes lets you group custom variable types and methods together and can define the functionality the object will have
+
+}
